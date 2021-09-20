b9b756729f7a99de0cf7e0434e0e37a9f53b01c3
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-09-20 16:52:48 -0300

    Primeira regra de negocio

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,16 +1,38 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public string Adress { get; set; }
+        private IList<Subscription> _subscriptions;
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Adress { get; private set; }
         //endereço de entrega do produto.
-        public List<Subscription> Subscriptions { get; set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
         //o aluno pode ter várias assinaturas, porém, só uma ativa.
+
+        public void AddSubscription(Subscription subscription)
+        {
+            //Regra de negócio: Cancela todas as outras assinaturas e coloca esta como principal.
+            foreach (var sub in Subscriptions)
+            {
+                sub.Active = false;
+            }
+
+            _subscriptions.Add(subscription);
+        }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription();
+            var student = new Student("Pedro", "Ferreira", "123456789", "pedro@gmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
